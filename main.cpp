f6752cc64a40b58e99e39838723026d398b16738
JefferyXie <jeffery.xie@gmail.com>
2016-01-31 22:13:53 -0600

    add article links for googletest

--- main.cpp
+++ main.cpp
@@ -3,6 +3,12 @@
 #include "./test/Test_language.h"
 #include "./test/Test_algorithm.h"
 
+//
+// googletest articles:
+// http://www.ibm.com/developerworks/aix/library/au-googletestingframework.html
+// http://www.yolinux.com/TUTORIALS/Cpp-GoogleTest.html
+// https://github.com/google/googletest/blob/master/googletest/docs/FAQ.md#why-is-it-not-recommended-to-install-a-pre-compiled-copy-of-google-test-for-example-into-usrlocal
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
