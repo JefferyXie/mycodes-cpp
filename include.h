f85c1a223844fb47942325b2418bb2b721de6b20
Jeffery Xie <jeffery.xie@morningstar.com>
2016-03-24 17:52:58 -0500

    refactor main.cpp

---
+++ include.h
@@ -0,0 +1,18 @@
+#ifndef INCLUDE_H
+#define INCLUDE_H
+
+#include <stdlib.h>
+#include <cstdio>
+#include <stdio.h>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+#endif
+
+
