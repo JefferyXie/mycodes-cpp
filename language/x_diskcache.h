d4afe42779722dafb7209e105d72e3607da44ae8
JefferyXie <jeffery.xie@gmail.com>
2016-07-21 23:06:46 -0500

    add file (links) about linux disk cache, should try the code

---
+++ language/x_diskcache.h
@@ -0,0 +1,12 @@
+#ifndef X_DISKCACHE_H
+#define X_DISKCACHE_H
+
+// 
+// Below article tells why linux "consume" so many memory even if no many
+// applications are running.
+// www.linuxatemyram.com/index.html
+// Below page gives great code example and analysis show the benefit of disk cache.
+// www.linuxatemyram.com/play.html
+
+#endif
+
