fac8de7eaed1d155ebd1de3dd2c11e237f762d45
JefferyXie <jeffery.xie@gmail.com>
2017-01-19 22:35:08 -0600

    IPC socket sample code

---
+++ XSocket/ipc_client.c
@@ -0,0 +1,104 @@
+#ifndef IPC_CLIENT_C
+#define IPC_CLIENT_C
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+//
+// https://users.cs.cf.ac.uk/Dave.Marshall/C/node28.html
+// unix domain sockets vs. internet sockets - 
+// https://lists.freebsd.org/pipermail/freebsd-performance/2005-February/001143.html
+//
+//
+#define NSTRS       3           /* no. of strings  */
+#define ADDRESS     "/home/jeffery/mysocket"  /* addr to connect */
+
+/*
+ * Strings we send to the server.
+ */
+char *strs[NSTRS] = {
+    "This is the first string from the client.\n",
+    "This is the second string from the client.\n",
+    "This is the third string from the client.\n"
+};
+
+int main()
+{
+    char c;
+    FILE *fp;
+    int i, s, len;
+    struct sockaddr_un saun;
+
+    /*
+     * Get a socket to work with.  This socket will
+     * be in the UNIX domain, and will be a
+     * stream socket.
+     */
+    if ((s = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) {
+        perror("client: socket");
+        exit(1);
+    }
+
+    /*
+     * Create the address we will be connecting to.
+     */
+    saun.sun_family = AF_UNIX;
+    strcpy(saun.sun_path, ADDRESS);
+
+    /*
+     * Try to connect to the address.  For this to
+     * succeed, the server must already have bound
+     * this address, and must have issued a listen()
+     * request.
+     *
+     * The third argument indicates the "length" of
+     * the structure, not just the length of the
+     * socket name.
+     */
+    len = sizeof(saun.sun_family) + strlen(saun.sun_path);
+
+    if (connect(s, (struct sockaddr*)&saun, len) < 0) {
+        perror("client: connect");
+        exit(1);
+    }
+
+    /*
+     * We'll use stdio for reading
+     * the socket.
+     */
+    fp = fdopen(s, "r");
+
+    /*
+     * First we read some strings from the server
+     * and print them out.
+     */
+    for (i = 0; i < NSTRS; i++) {
+        while ((c = fgetc(fp)) != EOF) {
+            putchar(c);
+
+            if (c == '\n')
+                break;
+        }
+    }
+
+    /*
+     * Now we send some strings to the server.
+     */
+    for (i = 0; i < NSTRS; i++)
+        send(s, strs[i], strlen(strs[i]), 0);
+
+    /*
+     * We can simply use close() to terminate the
+     * connection, since we're done with both sides.
+     */
+    close(s);
+
+    exit(0);
+}
+
+#endif
+
