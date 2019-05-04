6f811ff92b855c664ff9bfb50042202e05f21cc9
JefferyXie <jeffery.xie@gmail.com>
2019-05-03 23:48:11 -0500

    improve & bug fix lock_reader_writer

--- thread/lock_reader_writer.h
+++ thread/lock_reader_writer.h
@@ -2,6 +2,7 @@
 #define LOCK_READER_WRITER_H
 
 #include "../main/header.h"
+#include "thread_id.h"
 
 //
 /*
@@ -26,22 +27,61 @@
 class lock_reader_writer
 {
 public:
-    void read_lock()
-    {
+    struct scoped_reader_lock_t {
+        lock_reader_writer& _rw;
+        scoped_reader_lock_t() = delete;
+        scoped_reader_lock_t(const scoped_reader_lock_t&) = delete;
+        // mu must have been locked
+        scoped_reader_lock_t(lock_reader_writer& rw) : _rw(rw) {
+            _rw.read_lock();
+        }
+        ~scoped_reader_lock_t() {
+            _rw.read_unlock();
+        }
+    };
+
+    // 
+    // it's not encouraged to explictly call below lock/unlock interface
+    // instead, use SCOPED_RW_READ & SCOPED_RW_WRITE
+
+    void read_lock() {
         std::unique_lock<std::mutex> lock_read(_mu_read);
-        if (readers++ == 0)
+        if (_readers++ == 0)
         {
+            cout << "\n\t[" << gettid() << "] will lock write" << endl;
             _mu_write.lock();
+            _mu_write_owner_thread = gettid();
         }
     }
-    void read_unlock()
-    {
+    void read_unlock() {
         std::unique_lock<std::mutex> lock_read(_mu_read);
-        if (--readers == 0)
-        {
-            _mu_write.unlock();
+        if (gettid() == _mu_write_owner_thread) {
+            if (_readers == 1) {
+                --_readers;
+                _mu_write_owner_thread = 0;
+                _mu_write.unlock();
+            } else if (_readers > 1) {
+                // only the owner thread can unlock
+                // should wait until notified all other readers done
+                _cv.wait(lock_read, [this]() {
+                    return _readers == 1;
+                });
+
+                cout << "\n\t[" << gettid() << "] will unlock write" << endl;
+
+                --_readers;
+                _mu_write_owner_thread = 0;
+                _mu_write.unlock();
+            } else {
+                assert("Wrong _readers!");
+            }
+        } else {
+            if (--_readers == 1) {
+                _cv.notify_one();
+            }
         }
     }
+
     void write_lock()
     {
         _mu_write.lock();
@@ -51,65 +91,110 @@ public:
         _mu_write.unlock();
     }
 
+    // the old implementation of read_lock() & read_unlock() is wrong
+    // same mutext's unlock should be done by the thread who locked it
+    // otherwise the behavior is undefined per c++ standard
+    void WRONG_read_lock() {
+        std::unique_lock<std::mutex> lock_read(_mu_read);
+        if (_readers++ == 0)
+        {
+            cout << "\n\t[" << gettid() << "] will lock write" << endl;
+            _mu_write.lock();
+        }
+    }
+    void WRONG_read_unlock() {
+        std::unique_lock<std::mutex> lock_read(_mu_read);
+        if (--_readers == 0) {
+            cout << "\n\t[" << gettid() << "] will unlock write" << endl;
+            _mu_write.unlock();
+        }
+    }
+
+    std::mutex& get_mu_write() { return _mu_write; }
+
 private:
-    int         readers = 0;
+    int         _readers = 0;
+    unsigned    _mu_write_owner_thread = 0;
+    std::condition_variable _cv;
     std::mutex	_mu_read;
     std::mutex	_mu_write;
 };
 
-static lock_reader_writer s_locker;
+// TODO: is there a better way to define the macro??
+#define SCOPED_RW_READ(rw) lock_reader_writer::scoped_reader_lock_t __scoped_r_##rw(rw)
+#define SCOPED_RW_WRITE(rw) std::lock_guard<std::mutex> __scoped_w_##rw(rw.get_mu_write())
+
+lock_reader_writer s_locker;
+
 void* reader(void*);
 void* writer(void*);
 void run_lock_reader_writer()
 {  
-    int a = 1, b = 1;
-    int a1 = 2, b1 = 2;
+    int a = 0, b = 0;
+    int a1 = 1, b1 = 1;
+    int a2 = 2, b2 = 2;
+    int a3 = 3, b3 = 3;
+    int a4 = 4, b4 = 4;
     system("clear");
-    pthread_t r,w,r1,w1;
+    pthread_t r,w,r1,w1,r2,w2,r3,w3,r4,w4;
     pthread_create(&r,NULL,reader,(void*)&a);
     pthread_create(&w1,NULL,writer,(void*)&b);
     pthread_create(&r1,NULL,reader,(void*)&a1);
     pthread_create(&w,NULL,writer,(void*)&b1);
+    pthread_create(&r2,NULL,reader,(void*)&a2);
+    pthread_create(&w2,NULL,writer,(void*)&b2);
+    pthread_create(&r3,NULL,reader,(void*)&a3);
+    pthread_create(&w3,NULL,writer,(void*)&b3);
+    pthread_create(&r4,NULL,reader,(void*)&a4);
+    pthread_create(&w4,NULL,writer,(void*)&b4);
+
     pthread_join(r,NULL);
     pthread_join(w1,NULL);
     pthread_join(r1,NULL);
     pthread_join(w,NULL);
-    printf("run_lock_reader_writer ends.\n");
-    return 0;
+    pthread_join(w2,NULL);
+    pthread_join(r2,NULL);
+    pthread_join(w3,NULL);
+    pthread_join(r3,NULL);
+    pthread_join(w4,NULL);
+    pthread_join(r4,NULL);
+
+    printf("\n\nrun_lock_reader_writer ends.\n");
 }
 void* reader(void* arg)  
 {
     int c = *static_cast<int*>(arg);
-    printf("\nreader %d is created",c); sleep(1);
+    printf("\n[R]reader %d is created",c); sleep(1);
 
-    s_locker.read_lock();
+//    s_locker.read_lock();
+    SCOPED_RW_READ(s_locker);
 
     /*Critcal Section */
-    printf("\n\nreader %d is reading ", c);
+    printf("\n\n[R]reader %d is reading ", c);
     sleep(1);
-    printf("\nreader%d finished reading",c);
+    printf("\n[R]reader%d finished reading",c);
     /* critical section completd */
 
-    s_locker.read_unlock();
+//    s_locker.read_unlock();
 
     return 0;
 }
 void* writer(void* arg)
 {
     int c = *static_cast<int*>(arg);
-    printf("\nwriter %d is created", c);
+    printf("\n[W]writer %d is created", c);
     sleep(1);
 
-    s_locker.write_lock();
+//    s_locker.write_lock();
+    SCOPED_RW_WRITE(s_locker);
 
-    printf("\nwriter %d is writing", c);
+    printf("\n[W]writer %d is writing", c);
     sleep(1);
-    printf("\nwriter%d finished writing", c);
+    printf("\n[W]writer%d finished writing", c);
 
-    s_locker.write_unlock();
+//    s_locker.write_unlock();
 
     return 0;
 }
 
 #endif
-
